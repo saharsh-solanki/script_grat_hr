52856ee57955a54a8f649c8356041b4ce49adae2
Saharsh.Solanki <Saharsh.Solanki@healthplix.com>
2023-09-25 21:46:01 +0530

    test

--- selenium_script.py
+++ selenium_script.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
 display.start()
+import time
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,
@@ -35,9 +37,9 @@ driver = webdriver.Chrome(options = chrome_options)
 
 driver.get("https://cubexo-software.greythr.com/")
 
-username = driver.find_element_by_xpath("//input[@id='username']")
-password = driver.find_element_by_xpath("//input[@id='password']")
-login_button = driver.find_element_by_xpath("//button[text()=' Log in ']")
+username = driver.find_element(By.XPATH, "//input[@id='username']")
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+login_button = driver.find_element(By.XPATH, "//button[text()=' Log in ']")
 
 username.send_keys("CSS016")
 password.send_keys("s@S7223889629")
@@ -47,12 +49,12 @@ time.sleep(20)  # Wait for 20 seconds
 
 # Morning Scenario: Log In
 if time.localtime().tm_hour == 10:
-    sign_in_button = driver.find_element_by_xpath("//button[text()='Sign In']")
+    sign_in_button = driver.find_element(By.XPATH,"//button[text()='Sign In']")
     sign_in_button.click()
 
 # Evening Scenario: Log Out
 elif time.localtime().tm_hour == 20:
-    sign_out_button = driver.find_element_by_xpath("//button[text()='Sign Out']")
+    sign_out_button = driver.find_element(By.XPATH,"//button[text()='Sign Out']")
     sign_out_button.click()
 
 # Close the WebDriver
